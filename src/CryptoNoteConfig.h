de3c8e83e1969e8cf221e06627dbc63570f55cfd
waybow01 <ski-on@bigpond.net.au>
2018-03-11 09:35:39 +1000

    a dded new cmakelist

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-    "18.217.223.19:17236",
+    "18.220.220.18:17236",
     "18.222.80.60:17236"
 };
 
